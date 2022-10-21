9701be3579eb986ec4b41b957d03fe6d6ad40e62
Neka <benjamin.anezo@protonmail.com>
2022-10-21 14:36:33 +0200

    setup: transformation into a package

---
+++ dist/Button/Button.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var Button = function Button(_ref) {
+  var text = _ref.text;
+  return /*#__PURE__*/_react.default.createElement("button", null, text);
+};
+var _default = Button;
+exports.default = _default;
---
+++ dist/README.md
@@ -0,0 +1,2 @@
+# react-components-library
+We wanted to have basic components at hand so we decided to make a library of efficient 🚀 , smart 🧠 and customizable 🛠️ components
